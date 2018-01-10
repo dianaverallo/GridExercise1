79aa1dc43ec91e178de2899e727a44781f3ee0a2
Diana Verallo <001649@lrmc.com.ph>
2018-01-10 15:44:22 +0800

    Add project files.

---
+++ GridExercise1/GridExercise1.Android/CustomRenderer/CustomButton_Droid.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Button = Xamarin.Forms.Button;
+using Android.Content;
+using Android.Graphics.Drawables;
+using GridExercise1.Droid.CustomRenderer;
+using GridExercise1.CustomRenderer;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButton_Droid))]
+
+namespace GridExercise1.Droid.CustomRenderer
+{
+    class CustomButton_Droid : ButtonRenderer
+    {
+        public CustomButton_Droid (Context context) : base (context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null) return;
+            var gradientDrawable = new GradientDrawable();
+            gradientDrawable.SetShape(ShapeType.Rectangle);
+            gradientDrawable.SetColor(Element.BackgroundColor.ToAndroid(Color.FromRgb(255, 215, 0)));
+            gradientDrawable.SetStroke(4,Element.BorderColor.ToAndroid());
+            gradientDrawable.SetCornerRadius(30.0f);
+            Control.SetBackground(gradientDrawable);
+
+        }
+    }
+}
---
+++ GridExercise1/GridExercise1/CustomRenderer/CustomButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace GridExercise1.CustomRenderer
+{
+    public class CustomButton : Button
+    {
+
+    }
+}
